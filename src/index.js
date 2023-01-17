803dfd61f9a83ee683b013cdc946337983c62c8c
skayaBy <zavadskaya.ov@gmail.com>
2023-01-17 18:16:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n=Math.abs(n);
+    let str=String(n);
+    if (n%10==0) {
+        let temp=str.slice(0, -1);
+        return temp.split("").reverse().join("");
+    }
+    else {
+        return str.split("").reverse().join("");
+    }
 }
